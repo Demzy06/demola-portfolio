9fcd57ce63751577f82971e31f2636a6adc9be46
olayode demola <demolaolayode6@gmail.com>
2026-07-18 00:49:33 +0100

    Add component layout code and styling

--- src/components/TechStackSection.jsx
+++ src/components/TechStackSection.jsx
@@ -1,5 +1,28 @@
+const techStacks = [
+  "Html",
+  "Css",
+  "javaScript",
+  "React Js",
+  "Tailwind",
+  "Bootstrap",
+  "Styled Components",
+  "Motion",
+];
 function TechStackSection() {
-  return <div></div>;
+  return (
+    <section className="border-t border-[#EEEEEE] pt-10 pl-5 pr-5 mb-10">
+      <h4 className="uppercase text-[11px] font-bold tracking-[0.15rem] text-[#727272]">
+        Technical Stack
+      </h4>
+      <ul className="flex flex-wrap gap-x-8 mt-4">
+        {techStacks.map((stack) => (
+          <li className="text-[15px] text-secondary uppercase font-bold tracking-widest leading-12">
+            {stack}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
 
 export default TechStackSection;
