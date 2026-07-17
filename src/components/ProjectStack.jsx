4f327317827d983c25ae6c7ed6a2e8caccaafda7
olayode demola <demolaolayode6@gmail.com>
2026-07-17 15:28:15 +0100

    create component and layout

---
+++ src/components/ProjectStack.jsx
@@ -0,0 +1,15 @@
+function ProjectStack({ projectStack }) {
+  return (
+    <div className="mt-2 mb-5">
+      <ul className="flex">
+        {projectStack.map((stack) => (
+          <li className="pt-1 pb-1 p-3.5 h-fit font-bold tracking-widest rounded-full uppercase bg-[#EEEEEE] mr-2 text-[#727272] text-[10px] border border-[#cccbcb]">
+            {stack}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProjectStack;
