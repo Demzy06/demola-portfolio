371f8d34cbcd177013598018c8fcd207535d93e3
olayode demola <demolaolayode6@gmail.com>
2026-07-20 00:20:14 +0100

    import buttomjumpto component , delete unused code

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,9 @@
 import Button from "./Button";
 import BackdropImg from "../assets/backdrop-svg.svg";
+import ButtonJumpTo from "./ButtonJumpTo";
 function HeroSection() {
   return (
     <section className="mt-39 pl-5 pr-5 mb-15 ">
-      {/* <img
-        src={BackdropImg}
-        alt=""
-        srcset=""
-        className="absolute top-10 left-2.5 right-0 w-19  -z-1"
-      /> */}
       <h3 className="text-[18px] font-medium mb-2 text-secondary">
         Hey there, I'm-
       </h3>
@@ -24,16 +19,16 @@ function HeroSection() {
         I build fast, stunning websites and web application that drives business
         growth.
       </p>
-      <div className="flex justify-between mt-12 ">
-        <Button
-          // className="bg-black text-white pl-7 pr-7 p-3"
-          className="bg-black text-white w-[45%] text-center h-fit pt-3 pb-3"
+      <div className="flex justify-between mt-12">
+        <ButtonJumpTo
+          className="bg-black text-white w-[45%] text-center h-fit pt-3 pb-3  font-medium"
           text="Start a Project"
+          path="contact"
+          offset={-60}
         />
         <Button
-          // className="text-black pl-4 pr-4 p-3 bg-white border"
-          className="text-black w-[45%] p- bg-white border text-center pt-3 pb-3"
-          text="View My Work"
+          className="text-black w-[45%] bg-white border text-center pt-3 pb-3 font-medium"
+          text="Resume"
         />
       </div>
     </section>
