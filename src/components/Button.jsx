caeb8855cb11794a5b6bcaf031f244a6343a232b
olayode demola <demolaolayode6@gmail.com>
2026-07-19 16:49:43 +0100

    fixed layout bug

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,15 @@
 // import { Link } from "react-router-dom";
 
-function Button({ className, text, icon, iconStyles, path }) {
+function Button({ className, text, icon, path }) {
   return (
     <button className={`${className} ${icon && "flex"}`}>
       <a href={path} target="_blank" className={`${icon && "flex"}`}>
         {text}
         {
-          // icon && <span className="w-fit flex items-center ml-2">{icon}</span>
-          <div className="w-fit flex items-center ml-2">
-            <img src={icon} className={`${iconStyles}`} />
-          </div>
+          icon && <span className="w-fit flex items-center ml-2">{icon}</span>
+          // <span className="w-fit flex items-center ml-2">
+          //   <img src={icon} className={`${iconStyles}`} />
+          // </span>
         }
       </a>
     </button>
