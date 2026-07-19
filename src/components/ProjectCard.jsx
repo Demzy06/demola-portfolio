5aeb369ec7e47fe618ff028e2f98e478c05d89c7
olayode demola <demolaolayode6@gmail.com>
2026-07-19 16:50:28 +0100

    imported icons

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,11 @@
 import ProjectStack from "./ProjectStack";
 
 import Button from "./Button";
-import ArrowIcon from "../assets/icons/arrowIcon.png";
-import ShareBox from "../assets/icons/sharebox.webp";
+// import ArrowIcon from "../assets/icons/arrowIcon.png";
+// import ShareBox from "../assets/icons/sharebox.webp";
+
+import ArrowFilledIcon from "@iconify-react/weui/arrow-filled";
+import ShareBoxFillIcon from "@iconify-react/ri/share-box-fill";
 
 function ProjectCard({ project }) {
   return (
@@ -13,20 +16,30 @@ function ProjectCard({ project }) {
       <div>
         <ProjectStack projectStack={project.stack} />
         <h3 className="font-bold mb-8">{project.projectName}</h3>
-        <p className="mt-4 text-secondary">{project.projectHighlight}</p>
-        <div className="flex w-[80%] mt-4 mb-4">
+        <p className="mt-4 text-secondary ">{project.projectHighlight}</p>
+        <div className="flex w-[80%] mt-4 mb-4 ">
           <Button
             text="View Project"
             className="border-b border-[##818181] mr-6 pb-1.5"
-            icon={ArrowIcon}
-            iconStyles="w-3 h-fit"
+            icon={
+              <ArrowFilledIcon
+                height=""
+                style={{ color: "#818181", width: "12px", height: "fit" }}
+              />
+            }
+            // iconStyles="w-3 h-fit"
             path={project.link}
           />
           <Button
             text="Repo"
             className="border-b border-[##818181]  pb-1.5"
-            icon={ShareBox}
-            iconStyles="w-4 h-fit"
+            icon={
+              <ShareBoxFillIcon
+                height=""
+                style={{ color: "#818181", width: "16px" }}
+              />
+            }
+            // iconStyles="w-4 h-fit"
             path={project.repo}
           />
         </div>
