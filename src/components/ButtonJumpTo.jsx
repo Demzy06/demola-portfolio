a63b1240e907ef71b0059b113ce0e17df9829a77
olayode demola <demolaolayode6@gmail.com>
2026-07-20 00:25:15 +0100

    delete unused code

--- src/components/ButtonJumpTo.jsx
+++ src/components/ButtonJumpTo.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-scroll";
 function ButtonJumpTo({ className, text, path, offset }) {
   return (
     <button className={className}>
-      {/* <a href={path}>{text}</a> */}
       <Link to={path} smooth={true} duration={500} offset={offset}>
         {text}
       </Link>
