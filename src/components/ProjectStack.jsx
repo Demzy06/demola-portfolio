1bd36d315d4ee6120a670dc62653028821ee0b3f
olayode demola <demolaolayode6@gmail.com>
2026-07-20 00:11:56 +0100

    fix style bug

--- src/components/ProjectStack.jsx
+++ src/components/ProjectStack.jsx
@@ -1,7 +1,7 @@
 function ProjectStack({ projectStack }) {
   return (
     <div className="mt-2 mb-5">
-      <ul className="flex">
+      <ul className="flex flex-wrap">
         {projectStack.map((stack) => (
           <li className="pt-1 pb-1 p-3.5 h-fit font-bold tracking-widest rounded-full uppercase bg-[#EEEEEE] mr-2 text-[#727272] text-[10px] border border-[#cccbcb]">
             {stack}
