678ba54bb93d3dbd8e799765ed235df51342f746
olayode demola <demolaolayode6@gmail.com>
2026-07-20 01:39:32 +0100

    made chnages to the code, implmented the scroll to

--- src/components/ButtonJumpTo.jsx
+++ src/components/ButtonJumpTo.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-scroll";
 
 function ButtonJumpTo({ className, text, path, offset }) {
   return (
-    <button className={className}>
-      <Link to={path} smooth={true} duration={500} offset={offset}>
-        {text}
-      </Link>
-    </button>
+    <Link
+      to={path}
+      smooth={true}
+      duration={500}
+      offset={offset}
+      className={className}
+    >
+      <button>{text}</button>
+    </Link>
   );
 }
 
