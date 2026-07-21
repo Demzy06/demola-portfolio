bab6af7b8fce9a134138d74e234b68b7dee1dd22
olayode demola <demolaolayode6@gmail.com>
2026-07-21 01:37:44 +0100

    feat: implement functionality for scroll to section

--- src/components/TechStackSection.jsx
+++ src/components/TechStackSection.jsx
@@ -1,3 +1,5 @@
+import { Element } from "react-scroll";
+
 const techStacks = [
   "Html",
   "Css",
@@ -11,18 +13,20 @@ const techStacks = [
 ];
 function TechStackSection() {
   return (
-    <section className="border-t border-[#EEEEEE] pt-10 pl-5 pr-5 pb-10">
-      <h4 className="uppercase text-[11px] font-bold tracking-[0.15rem] text-[#727272]">
-        Technical Stack
-      </h4>
-      <ul className="flex flex-wrap gap-x-8 mt-4">
-        {techStacks.map((stack) => (
-          <li className="text-[15px] text-secondary uppercase font-bold tracking-widest leading-12">
-            {stack}
-          </li>
-        ))}
-      </ul>
-    </section>
+    <Element className="stacks">
+      <section className="border-t border-[#EEEEEE] pt-10 pl-5 pr-5 pb-10">
+        <h4 className="uppercase text-[11px] font-bold tracking-[0.15rem] text-[#727272]">
+          Technical Stack
+        </h4>
+        <ul className="flex flex-wrap gap-x-8 mt-4">
+          {techStacks.map((stack) => (
+            <li className="text-[15px] text-secondary uppercase font-bold tracking-widest leading-12">
+              {stack}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Element>
   );
 }
 
