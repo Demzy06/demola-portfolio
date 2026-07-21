2eabb1c16cd48bc57ee61fec91067ded6098fb93
olayode demola <demolaolayode6@gmail.com>
2026-07-21 01:38:39 +0100

    feat: implement scroll animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,19 @@
-import Button from "./Button";
-import BackdropImg from "../assets/backdrop-svg.svg";
+import { useInView } from "react-intersection-observer";
+
 import ButtonJumpTo from "./ButtonJumpTo";
+import Button from "./Button";
+
 function HeroSection() {
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  console.log(inView);
   return (
-    <section className="mt-39 pl-5 pr-5 mb-15 ">
-      <h3 className="text-[18px] font-medium mb-2 text-secondary">
+    <section
+      ref={ref}
+      className={` ${inView ? "animation-opacity-100" : "animation-opacity-0"} transition-all duration-950 mt-39 pl-5 pr-5 mb-15 `}
+    >
+      <h3 className="text-[18px] font-medium mb-2 text-secondary ">
         Hey there, I'm-
       </h3>
       <div>
@@ -15,11 +24,15 @@ function HeroSection() {
           I build things for the <span className="text-secondary">web.</span>
         </h1>
       </div>
-      <p className="mt-4 font-medium text-secondary">
+      <p
+        className={` ${inView ? "animation-opacity-100" : "animation-opacity-0"} transition-all duration-1150 mt-4 font-medium text-secondary`}
+      >
         I build fast, stunning websites and web application that drives business
         growth.
       </p>
-      <div className="flex justify-between mt-12">
+      <div
+        className={` ${inView ? "animation-opacity-100" : "animation-opacity-0"} transition-all duration-1300 flex justify-between mt-12`}
+      >
         <ButtonJumpTo
           className="bg-black text-white w-[45%] text-center h-fit pt-3 pb-3  font-medium"
           text="Start a Project"
