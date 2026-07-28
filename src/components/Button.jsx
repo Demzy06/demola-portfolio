0d997504bb75aac4051f82a7604dde5a2103dca0
olayode demola <demolaolayode6@gmail.com>
2026-07-28 13:55:17 +0100

    feat: implement onclick on the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-function Button({ className, text, icon, path }) {
+function Button({ className, text, icon, path, onClick }) {
   return (
-    <button className={`${className} ${icon && "flex"}`}>
+    <button className={`${className} ${icon && "flex"}`} onClick={onClick}>
       <a href={path} target="_blank" className={`${icon && "flex"}`}>
         {text}
         {icon && <span className="w-fit flex items-center ml-2">{icon}</span>}
