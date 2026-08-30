94ece92acff13a431f5ae23a2d907f32f4879a76
olayode demola <demolaolayode6@gmail.com>
2026-08-30 21:52:24 +0100

    style : add vertical gap

--- src/components/ProjectStack.jsx
+++ src/components/ProjectStack.jsx
@@ -1,7 +1,7 @@
 function ProjectStack({ projectStack }) {
   return (
     <div className="mt-2 mb-5">
-      <ul className="flex flex-wrap">
+      <ul className="flex flex-wrap gap-y-2">
         {projectStack.map((stack) => (
           <li className="pt-1 pb-1 p-3.5 h-fit font-bold tracking-widest rounded-full uppercase bg-[#EEEEEE] mr-2 text-[#727272] text-[10px] border border-[#cccbcb] md:mb-4">
             {stack}
